61427a15301ebc42e16f2d9568566013d5bea3c3
Hans Brems <hans.brems@longville-software.be>
2024-04-12 20:09:32 +0200

    Add Prose component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,43 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     fontFamily: {
-      sans: ["Noto Sans", "sans-serif"],
-      serif: ["IBM Plex Serif", "serif"],
+      sans: ['Noto Sans', 'sans-serif'],
+      serif: ['IBM Plex Serif', 'serif'],
     },
-    extend: {
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            h1: {
-              fontFamily: "IBM Plex Serif",
-            },
-            h2: {
-              fontFamily: "IBM Plex Serif",
-            },
-            h3: {
-              fontFamily: "IBM Plex Serif",
-            },
-            h4: {
-              fontFamily: "IBM Plex Serif",
-            },
-            h5: {
-              fontFamily: "IBM Plex Serif",
-            },
-            h6: {
-              fontFamily: "IBM Plex Serif",
-            },
-            a: {
-              color: theme("colors.green.500"),
-              "&:hover": {
-                color: theme("colors.green.600"),
-              },
-            },
-          },
-        },
-      }),
-    },
+    extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require('@tailwindcss/typography')],
 };
