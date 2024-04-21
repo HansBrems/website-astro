9fa707d4a39794db78bb53b25b68b0dc687f6a13
Hans Brems <hans.brems@longville-software.be>
2024-04-21 19:44:12 +0200

    Add Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
+import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://longville-software.be',
   integrations: [
@@ -9,5 +11,6 @@ export default defineConfig({
     sitemap({
       filter: (page) => page !== 'https://longville-software.be/dev/',
     }),
+    partytown({ forward: ['dataLayer.push'] }),
   ],
 });
