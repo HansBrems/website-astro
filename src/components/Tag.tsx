58c988b6e1a6041605cad161e42f1e5d8eb371ac
Hans Brems <hans.brems@longville-software.be>
2024-12-19 10:53:09 +0100

    Make tags stand out more

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -6,7 +6,7 @@ export interface Props {
 export default function Tag({ color, name }: Props) {
   return (
     <div
-      className={`flex items-center rounded-md border-b-[1px] border-solid bg-stone-50 p-2 text-xs font-bold text-stone-700 dark:bg-stone-800 dark:text-stone-100 border-${color}-500 dark:border-${color}-500 `}
+      className={`flex items-center rounded-md border-b-[1px] border-solid bg-stone-100 p-2 text-xs font-bold text-stone-700 dark:bg-stone-800 dark:text-stone-100 border-${color}-500 dark:border-${color}-500 `}
     >
       {name}
     </div>
